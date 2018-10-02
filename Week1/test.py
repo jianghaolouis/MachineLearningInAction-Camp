b47e8863a4c81be1c0074e57c91df68fbecd4419
jianghaolouis <Jianghao.liujiang@gmail.com>
2018-10-02 21:37:23 +0200

    manage folders

--- Week1/test.py
+++ Week1/test.py
@@ -1,3 +1,3 @@
 import kNN
-img_vector = kNN.img2vec('/Users/jianghao/Desktop/ML_in_action/MachineLearningInAction-Camp/Week1/digits/trainingDigits/0_4.txt')
-print(img_vector[1][1])
+img_vector = kNN.load_image('/Users/jianghao/Desktop/ML_in_action/MachineLearningInAction-Camp/Week1/digits/trainingDigits/0_4.txt')
+print(img_vector)
